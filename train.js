f88368f6c094f02494b167b3b4310a5d43400970
LEO <baxodirmamatvaliev@gmail.com>
2026-04-25 16:20:48 +0900

    feat: callback va asyny va MITASK A

--- train.js
+++ train.js
@@ -1,1 +1,113 @@
-console.log(" train task ishga tushdi !");
+// MITASK A
+
+let sana = 0;
+
+function num(gap, world) {
+  for (let i = 0; i < gap.length; i++) {
+    if (gap[i] === world) {
+      sana++;
+    }
+  }
+  return sana;
+}
+
+console.log(num("engineer", "e"));
+
+// console.log("BekzodRizo: Maslahatlari!");
+// const list = [
+//   "Yaxshi talaba bo'liing", // 0~20:
+//   "tog'ri boshliq tanlang va koproq xato qiling", // 20~30:
+//   "uzingizni ishlaringizni boshlang", // 30~40:
+//   "siz kuchli bo'lgan narsalarni qiling", // 40~50:
+//   "yoshlarga tafsiya bering", // 50~60":
+//   "endi dam oling , foydasi yoq endi", // 60~
+// ];
+
+// Node.js event loop VA CB function:
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 40) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+// console.log("passed hare 2");
+// maslahatBering(33, (err, data) => {
+//   if (err) console.log("ERROR", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed hare 1");
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 40) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
+// console.log("passed hare 2");
+// maslahatBering(33, (err, data) => {
+//   if (err) console.log("ERROR", err);
+//   console.log("javob:", data);
+
+// });
+// console.log("passed hare 1");
+
+// Asynchronous function:
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return [0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 40) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+// .then-catch: bu usul qiyin usul vs tartibsiz usul ekan / Call qismini.
+// console.log("passade hare 0");
+// maslahatBering(22)
+//   .then((data) => {
+//     maslahatBering(45)
+//       .then((data) => {
+//         console.log("Javob:", data);
+//       })
+//       .catch((err) => {
+//         console.log("ERROR:", err);
+//       });
+//     console.log("Javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passad hare 1");
+
+// async function run() {
+//   let javob = await maslahatBering(20);
+//   console.log(javob);
+//   javob = await maslahatBering(31);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();

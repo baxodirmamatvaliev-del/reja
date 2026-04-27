d00ae8bd6cd28e701a6eab441f66e8a955f7a540
LEO <baxodirmamatvaliev@gmail.com>
2026-04-27 19:29:54 +0900

    feat: MongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log(" web serverni boshlash: ");
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1: kirish code
+app.use(express.static("public")); // "tashqaridan kelayotgan saproslar va foydlanuvchilar uchun hizmat qiladi :"
+app.use(express.json()); // "krib kelayotgan datani OJBKECT ga ogirib beradi:"
+app.use(express.urlencoded({ extended: true })); // bu kod bn HTML TRADITIONAL formda yozilgan kodlani qabul qvoladi
+
+// 2: Session code
+
+// 3:  views code               BSS: beackend sayt rendring => ejs orqari Frond end ni yasemiz  beackend da. //
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: routing code
+// app.get("/hello", function (req, res) {
+//   res.end(` <h1>  HELLO WORLD </h1> `);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(` <h1> siz sovgalar bolimidasiz </h1> `);
+// });
+
+app.post("/create-item", (req, res) => {
+  // POST: o'zi bilan malumotli olib keladi datapase ga osha maumotni yozadi!
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  // get: ni malumot o'qish uchun ishlatamiz
+  res.render("reja");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,59 +1,30 @@
-console.log(" web serverni boshlash: ");
-const express = require("express");
-const app = express();
 const http = require("http");
+const mongodb = require("mongodb");
+const { connect } = require("http2");
 
-const fs = require("fs");
+let db;
+const connectionString =
+  "mongodb+srv://LEO:kpif2Ftqk0AHZ7s9@cluster0.tp9ctte.mongodb.net/ /reja?retryWrites=true&w=majority";
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
-
-// 1: kirish code
-app.use(express.static("public")); // "tashqaridan kelayotgan saproslar va foydlanuvchilar uchun hizmat qiladi :"
-app.use(express.json()); // "krib kelayotgan datani OJBKECT ga ogirib beradi:"
-app.use(express.urlencoded({ extended: true })); // bu kod bn HTML TRADITIONAL formda yozilgan kodlani qabul qvoladi
-
-// 2: Session code
-
-// 3:  views code               BSS: beackend sayt rendring => ejs orqari Frond end ni yasemiz  beackend da. //
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: routing code
-// app.get("/hello", function (req, res) {
-//   res.end(` <h1>  HELLO WORLD </h1> `);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(` <h1> siz sovgalar bolimidasiz </h1> `);
-// });
-
-app.post("/create-item", (req, res) => {
-  // POST: o'zi bilan malumotli olib keladi datapase ga osha maumotni yozadi!
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  // get: ni malumot o'qish uchun ishlatamiz
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    ` the server is running successfully on port ${PORT}, http://localhost:${PORT} `,
-  );
-});
+mongodb.connect(
+  connectionString,
+  {
+    UseNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR: ON CONNECTION MongoDB");
+    else {
+      console.log("MongoDB connecton secceed :)");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          ` the server is running successfully on port ${PORT}, http://localhost:${PORT} `,
+        );
+      });
+    }
+  },
+);
